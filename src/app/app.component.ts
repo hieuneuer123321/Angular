af26ae2c968124aff9d930015bc5d9387f9fd171
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-11-17 16:55:56 +0700

    Test Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  name = '';
+  listString: Array<string> = ['String 1', 'String 2', 'String 3'];
+  constructor() {}
+  @ViewChild('form') mytemplateForm!: NgForm;
+  functionName() {
+    this.listString.push(this.name);
+    console.log(this.listString);
+    console.log(this.mytemplateForm);
+    this.mytemplateForm.reset();
+  }
 }
